6ad6dcaa5a92287d1fd0965149365b62dce9819d
Peter-J-Sung <ivsinji746@naver.com>
2024-04-07 22:54:44 +0900

    [PURPLE-49]: media query applied (#8)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+import "./global.css";
 import type { Metadata } from "next";
 import ReactQueryProviders from "@/provider/reactQueryProviders";
 import MuiProvider from "@/provider/muiProvider";
 import { ReactNode } from "react";
 import { Body, HTML } from "@/component/atom/BoxComponents";
+import { CssBaseline } from "@mui/material";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <HTML lang="en" height={"100%"}>
       <Body height={"100%"}>
         <ReactQueryProviders>
+          <CssBaseline />
           <MuiProvider>{children}</MuiProvider>
         </ReactQueryProviders>
       </Body>
