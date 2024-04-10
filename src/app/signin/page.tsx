fd7aa4d90cc54000848b77680e93b06f09cde61c
Peter-J-Sung <ivsinji746@naver.com>
2024-04-10 20:08:18 +0900

    refactor kakao login button

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,12 +2,13 @@
 
 import OAuthLoginButton from "@/component/atom/OAuthLoginButton";
 import { S } from "./styles";
+import { OauthType } from "@/app/constant/session";
 
-export default function SignIn() {
+export default function SignInPage() {
   return (
     <S.Wrapper>
       <div>Img</div>
-      <OAuthLoginButton />
+      <OAuthLoginButton type={OauthType.KAKAO} />
     </S.Wrapper>
   );
 }
