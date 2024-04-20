64ec8de1230d12da59c064d7ef4d8446c5229aa6
seonyeong719 <sy904280@gmail.com>
2024-04-20 19:04:23 +0900

    Feat: 폴더 import 삭제

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,7 @@
-import PublicBanners from "@/component/banner";
-
 export default function SignUpPage() {
   return (
     <div>
       <div>SignUp Page</div>
-      <PublicBanners />
     </div>
   );
 }
