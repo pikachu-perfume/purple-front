655400940fad562fb5bfb55de1541afd963c0049
seonyeong719 <sy904280@gmail.com>
2024-04-24 17:05:16 +0900

    Feat: 배너 폴더 구조 변경

--- src/components/banner/styles.ts
+++ src/components/banner/styles.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
 
 const Wrapper = styled.div`
   display: flex;
@@ -7,31 +8,33 @@ const Wrapper = styled.div`
   justify-content: center;
   background-color: white;
   border-radius: 0.6rem;
+  overflow: hidden;
   width: 100%;
   max-width: 36rem;
 `;
 
 const BannerImg = styled.img`
-  height: 25rem;
-  border-radius: 0.6rem 0.6rem 0 0;
+  aspect-ratio: 1.44;
   width: 100%;
 `;
 
 const ContentWrap = styled.div`
-  padding: 1rem;
   height: 14.2rem;
+  padding: 1.6rem 2rem;
+  display: flex;
+  flex-direction: column;
 `;
 
-const TopContent = styled.div`
-  margin: 1rem 0;
-  & > span {
-    color: #9091a0;
-  }
-`;
-
-const Title = styled.div`
-  font-size: 1rem;
+const Title = styled(Typography)`
+  font-size: 1.8rem;
+  line-height: 2.1rem;
   font-weight: 700;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const Score = styled.div`
@@ -49,6 +52,9 @@ const CategoryWrap = styled.div`
 `;
 
 const BannerInfo = styled.div`
+  margin-top: auto;
+  font-size: 1.4rem;
+  line-height: 2.2rem;
   word-wrap: break-word;
   display: -webkit-box;
   -webkit-line-clamp: 2;
@@ -61,7 +67,6 @@ export const S = {
   Wrapper,
   BannerImg,
   ContentWrap,
-  TopContent,
   BannerInfo,
   Title,
   Score,
