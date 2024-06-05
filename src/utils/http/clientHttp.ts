69cb22c2cb6f912c9d794005a46ab1a60b7bc0e5
Peter-J-Sung <ivsinji746@naver.com>
2024-06-06 03:47:27 +0900

    handle 401 error

--- src/utils/http/clientHttp.ts
+++ src/utils/http/clientHttp.ts
@@ -7,6 +7,14 @@ const clientHttp = axios.create({
 });
 
 clientHttp.interceptors.request.use(httpConfigHelper, () => {});
-clientHttp.interceptors.response.use(httpParserHelper, () => {});
+clientHttp.interceptors.response.use(httpParserHelper, error => {
+  /*
+   * if(error.status === 401) {
+   *   window.localStorage.removeItem(TOKEN_SAVE_KEY);
+   * }
+   * 401(아마도) 에러 발생했을때 로컬 스토리지에서 jwt 토큰삭제.
+   * UI 적인건 개별 컴포넌트 혹은 페이지에서 처리.
+   * */
+});
 
 export default clientHttp;
