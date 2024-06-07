5feeb3d2e1c7a522a006be31c02e86ba29fb89e9
seonyeong719 <sy904280@gmail.com>
2024-06-07 23:50:07 +0900

    Feat: commentPage 제작중

--- src/app/commentPage/page.tsx
+++ src/app/commentPage/page.tsx
@@ -4,6 +4,7 @@ import NavHeader from "@/components/navHeaderLayout/navHeaderLayout";
 import { S } from "./styles";
 import Product from "./_components/Product/Product";
 import { CommentType } from "@/constant/comment";
+import Rating from "@/components/atom/Rating/Rating";
 
 const CommentPage = () => {
   return (
@@ -23,10 +24,25 @@ const CommentPage = () => {
       <S.Wrapper>
         <Product />
         <S.CommentWrap>
-          {Object.values(CommentType).map((el, idx) => (
-            <div key={idx}>{el}</div>
+          {Object.values(CommentType).map((comment, idx) => (
+            <div key={idx}>{comment}</div>
           ))}
         </S.CommentWrap>
+        <S.EvaluationWrap>
+          <span>이 향수 어때요?</span>
+          <S.RatingWrap>
+            <Rating precision={0.1} />
+            <div>향이 마음에 들어요</div>
+          </S.RatingWrap>
+        </S.EvaluationWrap>
+        <S.ReviewWrap>
+          <S.ReviewTitle>
+            <span>리뷰 한줄평</span>
+            <div>*</div>
+          </S.ReviewTitle>
+          <textarea></textarea>
+        </S.ReviewWrap>
+        <button>코멘트 등록</button>
       </S.Wrapper>
     </>
   );
--- src/app/commentPage/styles.ts
+++ src/app/commentPage/styles.ts
@@ -6,18 +6,66 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: red;
+  padding: 1rem 2rem;
 `;
 
 const CommentWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: pink;
+  background-color: ${theme.color.grayColor[500]};
   height: 4.6rem;
   width: 22.3rem;
   border-radius: 2.4rem;
   font-size: ${theme.fontSize.sm};
+  margin: 2rem 0;
+
+  & > div {
+    padding: 1rem;
+  }
 `;
 
-export const S = { Wrapper, CommentWrap };
+const EvaluationWrap = styled.div`
+  width: 100%;
+  margin: 2rem 0;
+
+  & > span {
+    font-size: ${theme.fontSize.base};
+    font-weight: ${theme.fontWeight.semiBold};
+  }
+`;
+
+const RatingWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ReviewWrap = styled.div`
+  width: 100%;
+
+  & > textarea {
+    width: 100%;
+  }
+`;
+
+const ReviewTitle = styled.div`
+  display: flex;
+  font-size: ${theme.fontSize.base};
+  font-weight: ${theme.fontWeight.semiBold};
+
+  & > div {
+    color: red;
+    margin-left: 0.4rem;
+  }
+`;
+
+export const S = {
+  Wrapper,
+  CommentWrap,
+  EvaluationWrap,
+  RatingWrap,
+  ReviewWrap,
+  ReviewTitle,
+};
